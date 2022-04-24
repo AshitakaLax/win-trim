abdffa7bb0d69c68d5990405aea560b9bde37ceb
Levi Balling <leviballing@gmail.com>
2022-04-24 11:32:49 -0600

    The initial additional of the source, this isn't functioning yet, but it has the core of most of what I need. There is a lot of addition features I can add to this. this is just what I have within a few hours.

---
+++ win-trim/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace win_trim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DirectoryButton = new System.Windows.Forms.Button();
+            this.FileListBox = new System.Windows.Forms.ListBox();
+            this.StatsLabel = new System.Windows.Forms.Label();
+            this.player2 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.QueueButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DirectoryButton
+            // 
+            this.DirectoryButton.Location = new System.Drawing.Point(12, 13);
+            this.DirectoryButton.Name = "DirectoryButton";
+            this.DirectoryButton.Size = new System.Drawing.Size(116, 23);
+            this.DirectoryButton.TabIndex = 2;
+            this.DirectoryButton.Text = "Select Directory";
+            this.DirectoryButton.UseVisualStyleBackColor = true;
+            this.DirectoryButton.Click += new System.EventHandler(this.DirectoryButton_Click);
+            // 
+            // FileListBox
+            // 
+            this.FileListBox.FormattingEnabled = true;
+            this.FileListBox.Location = new System.Drawing.Point(12, 54);
+            this.FileListBox.Name = "FileListBox";
+            this.FileListBox.Size = new System.Drawing.Size(120, 264);
+            this.FileListBox.TabIndex = 3;
+            this.FileListBox.SelectedIndexChanged += new System.EventHandler(this.FileListBox_SelectedIndexChanged);
+            // 
+            // StatsLabel
+            // 
+            this.StatsLabel.AutoSize = true;
+            this.StatsLabel.Location = new System.Drawing.Point(13, 325);
+            this.StatsLabel.Name = "StatsLabel";
+            this.StatsLabel.Size = new System.Drawing.Size(31, 13);
+            this.StatsLabel.TabIndex = 4;
+            this.StatsLabel.Text = "Stats";
+            // 
+            // player2
+            // 
+            this.player2.Enabled = true;
+            this.player2.Location = new System.Drawing.Point(537, -1);
+            this.player2.Margin = new System.Windows.Forms.Padding(1);
+            this.player2.Name = "player2";
+            this.player2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player2.OcxState")));
+            this.player2.Size = new System.Drawing.Size(375, 326);
+            this.player2.TabIndex = 1;
+            // 
+            // player
+            // 
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(160, -1);
+            this.player.Margin = new System.Windows.Forms.Padding(1);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(375, 326);
+            this.player.TabIndex = 0;
+            this.player.KeyPressEvent += new AxWMPLib._WMPOCXEvents_KeyPressEventHandler(this.player_KeyPressEvent);
+            // 
+            // QueueButton
+            // 
+            this.QueueButton.Location = new System.Drawing.Point(160, 329);
+            this.QueueButton.Name = "QueueButton";
+            this.QueueButton.Size = new System.Drawing.Size(116, 23);
+            this.QueueButton.TabIndex = 5;
+            this.QueueButton.Text = "Queue Job";
+            this.QueueButton.UseVisualStyleBackColor = true;
+            this.QueueButton.Click += new System.EventHandler(this.QueueButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 392);
+            this.Controls.Add(this.QueueButton);
+            this.Controls.Add(this.StatsLabel);
+            this.Controls.Add(this.FileListBox);
+            this.Controls.Add(this.DirectoryButton);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer player;
+        private AxWMPLib.AxWindowsMediaPlayer player2;
+        private System.Windows.Forms.Button DirectoryButton;
+        private System.Windows.Forms.ListBox FileListBox;
+        private System.Windows.Forms.Label StatsLabel;
+        private System.Windows.Forms.Button QueueButton;
+    }
+}
+
---
+++ win-trim/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AxWMPLib;
+
+namespace win_trim
+{
+    public partial class Form1 : Form
+    {
+        private string SelectedFile;
+        public Form1()
+        {
+            InitializeComponent();
+
+            player.settings.autoStart = false;
+            player2.settings.autoStart = false;
+            player2.CurrentMediaItemAvailable += Player_CurrentMediaItemAvailable;
+        }
+
+        private void Player_CurrentMediaItemAvailable(object sender, AxWMPLib._WMPOCXEvents_CurrentMediaItemAvailableEvent e)
+        {
+            player2.Ctlcontrols.currentPosition = player2.currentMedia.duration - 60.00;
+
+        }
+
+        private void DirectoryButton_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                FileListBox.Items.Clear();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath, "*.mp4");
+                    FileListBox.Items.AddRange(files);
+                    //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                }
+            }
+        }
+
+        private void QueueButton_Click(object sender, EventArgs e)
+        {
+            // Fetch the start time and end time
+            double trimStartSeconds = player.Ctlcontrols.currentPosition;
+            double trimEndSeconds = player2.Ctlcontrols.currentPosition;
+            TimeSpan endTime = TimeSpan.FromSeconds(trimEndSeconds);
+            // close out the media players
+            player.Ctlcontrols.stop();
+            player2.Ctlcontrols.stop();
+
+            // start a new thread to create the new video file
+
+            var temp = FFmpeg.Conversions.New()
+                .SetSeek(TimeSpan.FromSeconds(trimStartSeconds))
+                .AddParameter($"-to {endTime.ToString("hh:mm:ss")}")
+                .SetOutput($"temp-{SelectedFile}")
+                .Start();
+            // replace the original file with the new video file.
+        }
+
+        private void FileListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectedFile = (string)FileListBox.SelectedItem;
+            player.URL = SelectedFile;
+            player2.URL = SelectedFile;
+
+            StatsLabel.Text = $"STATS: {SelectedFile} \nDuration:{player.currentMedia.duration}";
+            player.Ctlcontrols.currentPosition = 60.00;
+        }
+
+        private void player_KeyPressEvent(object sender, AxWMPLib._WMPOCXEvents_KeyPressEvent e)
+        {
+            if (e.nKeyAscii == 0)
+            {
+                player.Ctlcontrols.currentPosition = player.Ctlcontrols.currentPosition + 10;
+            }
+        }
+    }
+}
